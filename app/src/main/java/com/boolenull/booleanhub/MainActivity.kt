e1a83edccd744f6fc658df2d381167d8ebc08b73
booleanull <booleanull@gmail.com>
2019-04-07 00:35:30 +0300

    activity

--- app/src/main/java/com/boolenull/booleanhub/MainActivity.kt
+++ app/src/main/java/com/boolenull/booleanhub/MainActivity.kt
@@ -1,13 +1,46 @@
 package com.boolenull.booleanhub
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import com.boolenull.booleanhub.fragment.BlogFragment
+import com.boolenull.booleanhub.fragment.LinkFragment
+import com.boolenull.booleanhub.fragment.RepositoryFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity: AppCompatActivity() {
+
+    private val onNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_blog -> {
+                    setFragment(BlogFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_repository -> {
+                    setFragment(LinkFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_aboutme -> {
+                    setFragment(RepositoryFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.content, fragment)
+            .commit()
     }
 }
 
--- app/src/main/java/com/boolenull/booleanhub/fragment/BlogFragment.kt
+++ app/src/main/java/com/boolenull/booleanhub/fragment/BlogFragment.kt
@@ -7,10 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import com.boolenull.booleanhub.R
 
-class InfoFragment: Fragment() {
+class BlogFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_info, container, false)
+        val view = inflater.inflate(R.layout.fragment_blog, container, false)
         return view
     }
 }
